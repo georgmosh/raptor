c4ac20a782661796bcf523fb0b644131a3ccd003
georgmosh <georgiosmoshovis@gmail.com>
2024-04-29 19:45:31 +0300

    Refactor demo.

---
+++ examples.py
@@ -0,0 +1,80 @@
+import os
+from raptor import RetrievalAugmentation, RetrievalAugmentationConfig
+from raptor.cluster_tree_builder import ClusterSpatialConfig, ClusterHardConfig
+from raptor.SummarizationModels import BlindSummarizationReverseModel
+
+os.environ["OPENAI_API_KEY"] = "I_LOVE_LASAGNA"
+os.environ["CLUSTER_MAXIMUM_SIZE"] = "10"
+
+# Cinderella story defined in sample.txt
+with open('demo/sample.txt', 'r') as file:
+    text = file.read()
+
+tree_builder_config = ClusterSpatialConfig(
+    tokenizer=None,
+    max_tokens=100,
+    num_layers=5,
+    threshold=0.5,
+    top_k=5,
+    selection_mode="top_k",
+    summarization_length=100,
+    summarization_model=None,
+    embedding_models=None,
+    cluster_embedding_model="OpenAI",
+)
+
+retrieval_augmentation_config = RetrievalAugmentationConfig(tree_builder_config=tree_builder_config)
+RA = RetrievalAugmentation(config=retrieval_augmentation_config)
+
+# construct the tree
+RA.add_documents(text)
+
+question = "How did Cinderella reach her happy ending ?"
+answer = RA.answer_question(question=question)
+print("Answer: ", answer)
+
+tree_builder_config = ClusterHardConfig(
+    tokenizer=None,
+    max_tokens=100,
+    num_layers=5,
+    threshold=0.5,
+    top_k=5,
+    selection_mode="top_k",
+    summarization_length=100,
+    summarization_model=None,
+    embedding_models=None,
+    cluster_embedding_model="OpenAI",
+)
+
+retrieval_augmentation_config = RetrievalAugmentationConfig(tree_builder_config=tree_builder_config)
+RA = RetrievalAugmentation(config=retrieval_augmentation_config)
+
+# construct the tree
+RA.add_documents(text)
+
+question = "How did Cinderella reach her happy ending ?"
+answer = RA.answer_question(question=question)
+print("Answer: ", answer)
+
+tree_builder_config = ClusterSpatialConfig(
+    tokenizer=None,
+    max_tokens=100,
+    num_layers=5,
+    threshold=0.5,
+    top_k=5,
+    selection_mode="top_k",
+    summarization_length=100,
+    summarization_model=BlindSummarizationReverseModel(),
+    embedding_models=None,
+    cluster_embedding_model="OpenAI",
+)
+
+retrieval_augmentation_config = RetrievalAugmentationConfig(tree_builder_config=tree_builder_config)
+RA = RetrievalAugmentation(config=retrieval_augmentation_config)
+
+# construct the tree
+RA.add_documents(text)
+
+question = "How did Cinderella reach her happy ending ?"
+answer = RA.answer_question(question=question)
+print("Answer: ", answer)
